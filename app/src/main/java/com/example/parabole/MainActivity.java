29928e3cb648a6947a410af11de5468776739120
Andriy <ak8022@bs.amalnet.k12.il>
2020-05-01 17:12:01 +0300

    On Click

--- app/src/main/java/com/example/parabole/MainActivity.java
+++ app/src/main/java/com/example/parabole/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.parabole;
         import androidx.appcompat.app.AppCompatActivity;
 
         import android.os.Bundle;
+        import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonPressed(View view)
+    {
+
     }
 }
