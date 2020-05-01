38274f899403eaee108e471866106b230d1b8c45
Andriy <ak8022@bs.amalnet.k12.il>
2020-05-01 17:18:44 +0300

    Linking components

--- app/src/main/java/com/example/parabole/MainActivity.java
+++ app/src/main/java/com/example/parabole/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.parabole;
 
-        import androidx.appcompat.app.AppCompatActivity;
-
-        import android.os.Bundle;
-        import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
 {
+    EditText etA;
+    EditText etB;
+    EditText etC;
+    WebView wv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etA=(EditText)findViewById(R.id.editText);
+        etB=(EditText)findViewById(R.id.editText2);
+        etC=(EditText)findViewById(R.id.editText3);
+        wv=(WebView)findViewById(R.id.myWebView);
     }
 
     public void buttonPressed(View view)
