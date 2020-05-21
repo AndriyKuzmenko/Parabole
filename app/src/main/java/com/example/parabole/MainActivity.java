6ff98c5aa8633b4b65c08dadb2f9dbb2d9ad212c
Andriy <ak8022@bs.amalnet.k12.il>
2020-05-21 16:47:01 +0300

    Meaningful names

--- app/src/main/java/com/example/parabole/MainActivity.java
+++ app/src/main/java/com/example/parabole/MainActivity.java
@@ -32,36 +32,36 @@ public class MainActivity extends AppCompatActivity
 
     public void buttonPressed(View view)
     {
-        String s1=etA.getText().toString();
-        String s2=etB.getText().toString();
-        String s3=etC.getText().toString();
+        String aString=etA.getText().toString();
+        String bString=etB.getText().toString();
+        String cString=etC.getText().toString();
 
-        if (s1==null || s2==null || s3==null || s1.equals("") || s2.equals("") || s3.equals(""))
+        if (aString==null || bString==null || cString==null || aString.equals("") || bString.equals("") || cString.equals(""))
         {
             Toast.makeText(getApplicationContext(), "Enter all the numbers!!!!", Toast.LENGTH_LONG).show();
             return;
         }
 
-        if (Double.valueOf(s1).doubleValue()==0)
+        if (Double.valueOf(aString).doubleValue()==0)
         {
             Toast.makeText(getApplicationContext(), "A can't be equal to 0!!!!!!!!!!!!!!!", Toast.LENGTH_LONG).show();
             return;
         }
 
-        double a=Double.valueOf(s1).doubleValue();
-        double b=Double.valueOf(s2).doubleValue();
-        double c=Double.valueOf(s3).doubleValue();
+        double a=Double.valueOf(aString).doubleValue();
+        double b=Double.valueOf(bString).doubleValue();
+        double c=Double.valueOf(cString).doubleValue();
 
         String equation="https://www.google.com/search?q=";
         if (a!=0 && a!=1 && b!=0 && b!=1 && c!=0 && c!=1)
         {
-            equation = "https://www.google.com/search?q=" + s1 + "x%5E2%2B" + s2 + "x%2B" + s3 + "&aqs=chrome.0.69i59j0l7.3454j0j9&sourceid=chrome&ie=UTF-8";
+            equation = "https://www.google.com/search?q=" + aString + "x%5E2%2B" + bString + "x%2B" + cString + "&aqs=chrome.0.69i59j0l7.3454j0j9&sourceid=chrome&ie=UTF-8";
             wv.loadUrl(equation);
         }
 
         if (a!=1)
         {
-            equation+=s1;
+            equation+=aString;
         }
         equation+="x%5E2%2B";
 
@@ -71,10 +71,10 @@ public class MainActivity extends AppCompatActivity
         }
         else if (b!=0)
         {
-            equation+=s2+"x%2B";
+            equation+=bString+"x%2B";
         }
 
-        equation+=s3;
+        equation+=cString;
         wv.loadUrl(equation);
     }
 
